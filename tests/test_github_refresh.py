0362f397e32bbb62a275f0c330714172a95a69c5
Michal Srb <michal@redhat.com>
2018-10-16 10:52:13 +0200

    Initial version

---
+++ tests/test_github_refresh.py
@@ -0,0 +1,24 @@
+"""Test github_refresh.py script."""
+
+from github_refresh import run, TASK_NAMES
+
+
+def test_run(mocker, db_results):
+    """Test the script."""
+    count_mock = mocker.patch('github_refresh._get_count')
+    count_mock.return_value = 1
+
+    outdated_mock = mocker.patch('github_refresh._get_outdated')
+    outdated_mock.return_value = db_results
+
+    schedule_refresh_mock = mocker.patch('github_refresh.run_flow_selective')
+
+    run(None)
+
+    schedule_refresh_mock.assert_called_once_with(
+        'bayesianPackageFlow',
+        TASK_NAMES,
+        {'ecosystem': 'maven', 'package': 'net.iharder:base64', 'force': True},
+        False,
+        False
+    )
