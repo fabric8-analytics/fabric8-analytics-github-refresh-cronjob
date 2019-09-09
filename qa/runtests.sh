f5a23e04909a9109d6af1063f45c715acb471bf8
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-09-09 14:10:37 +0200

    Updated the script runtests.sh to be able to run from qa subdirectory

--- qa/runtests.sh
+++ qa/runtests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+pushd "${SCRIPT_DIR}/.." > /dev/null
+
 set -e
 set -x
 
@@ -43,3 +47,5 @@ $(which pip3) install pytest-cov
 PYTHONDONTWRITEBYTECODE=1 PYTHONPATH=$(pwd) python3 "$(which pytest)" --cov=. --cov-config .coveragerc --cov-report term-missing --cov-fail-under=$COVERAGE_THRESHOLD -vv tests/
 codecov --token=c7d85992-337d-48b1-abb4-e55429b3e9c9
 printf "%stests passed%s\n\n" "${GREEN}" "${NORMAL}"
+
+popd > /dev/null
